be58f1b0fe8363feb764bee0bf785d684ff21320
Francisco Demartino <demartino.francisco@gmail.com>
2021-04-14 11:52:39 -0300

    tests pasan

--- parser/Parser.ts
+++ parser/Parser.ts
@@ -1,10 +1,82 @@
-// TODO: tipo del tad
+type ExpresionLogica = any;  // TODO
 
-export function parseTad(source: string) {
-    return {
-        nombre: "Foo",
-        generos: ["foo"],
+type Operacion = [string, string[], string, ExpresionLogica?];
+
+type TAD = {
+    nombre: string;
+    generos: string[];
+    generadores: Operacion[];
+    otrasOperaciones: Operacion[];
+};
+
+export function parseTad(source: string) : TAD {
+    let tad: TAD = {
+        nombre: "",
+        generos: [],
         generadores: [],
         otrasOperaciones: [],
+    };
+
+    let mode = "base";
+    let target = undefined;
+
+    source.split('\n').forEach((line) => {
+        line = line.split('--')[0].trim()
+
+        if (mode === "base") {
+            if (line.startsWith("TAD ")) {
+                tad.nombre = line.split("TAD ")[1];
+            }
+
+            if (line.startsWith("géneros ")) {
+                let gens = line.split(' ');
+                gens.shift();
+
+                tad.generos = gens;
+            }
+
+            if (line.startsWith("generadores")) {
+                mode = "operacion";
+                target = tad.generadores;
+            }
+
+            if (line.startsWith("otras operaciones")) {
+                mode = "operacion";
+                target = tad.otrasOperaciones;
+            }
+        } else if (mode === "operacion") {
+            if (line.trim() === "") {
+                mode = "base";
+            } else {
+                target.push(parseOperacion(line));
+            }
+        }
+
+    })
+
+    return tad;
+}
+
+function parseOperacion(line: string) : Operacion {
+    let [_name, line2] = line.split(":")
+    let [_args, line3] = line2.split("→")
+    let [_ret, ...rest] = line3.trim().split(" ")
+    let restr = null  // TODO
+
+    let name = _name.trim()
+
+    if (name.includes("•")) {
+        let parts = name.split("•").map((name) => name.trim()).filter((name) => name !== "")
+
+        if (parts.length === 1){
+            name = parts[0]
+        } else if (parts.length > 0) {
+            name = parts.join("_")
+        }
     }
+
+    let args = _args.split("×").map((name) => name.trim()).filter((name) => name !== "")
+    const ret = _ret.trim()
+
+    return [name, args, ret, restr]
 }
--- tests/ParseBool.test.ts
+++ tests/ParseBool.test.ts
@@ -7,23 +7,23 @@ const BOOL_TAD = fs.readFileSync(path.resolve(__dirname, "../tads/bool.tad"), 'u
 test("parsea bool", () => {
     const tad = parseTad(BOOL_TAD);
 
-    expect(tad.nombre).toBe("Bool")
-    expect(tad.generos).toBe(["bool"]);
+    expect(tad.nombre).toStrictEqual("Bool")
+    expect(tad.generos).toStrictEqual(["bool"]);
 
     // TODO: usar algo mejor tipado que una lista de 4 cosas?
 
-    expect(tad.generadores).toContain(["true", [], "bool", null]);
-    expect(tad.generadores).toContain(["false", [], "bool", null])
+    expect(tad.generadores).toContainEqual(["true", [], "bool", null]);
+    expect(tad.generadores).toContainEqual(["false", [], "bool", null])
 
-    expect(tad.otrasOperaciones).toContain(["if_then_else_fi", ["bool", "α", "α"], "α", null])
+    expect(tad.otrasOperaciones).toContainEqual(["if_then_else_fi", ["bool", "α", "α"], "α", null])
 
-    expect(tad.otrasOperaciones).toContain(["¬", ["bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["¬", ["bool"], "bool", null])
 
-    expect(tad.otrasOperaciones).toContain(["∨", ["bool", "bool"], "bool", null])
-    expect(tad.otrasOperaciones).toContain(["∧", ["bool", "bool"], "bool", null])
-    expect(tad.otrasOperaciones).toContain(["⇒", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["∨", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["∧", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["⇒", ["bool", "bool"], "bool", null])
 
-    expect(tad.otrasOperaciones).toContain(["∨L", ["bool", "bool"], "bool", null])
-    expect(tad.otrasOperaciones).toContain(["∧L", ["bool", "bool"], "bool", null])
-    expect(tad.otrasOperaciones).toContain(["⇒L", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["∨L", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["∧L", ["bool", "bool"], "bool", null])
+    expect(tad.otrasOperaciones).toContainEqual(["⇒L", ["bool", "bool"], "bool", null])
 })
