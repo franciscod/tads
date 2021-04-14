ff387dffdf5a2dff289ff7590d1ced81f27aecc6
Martín Lombardo <to@mlomb.me>
2021-04-14 10:40:05 -0300

    webpack skeleton

---
+++ site/index.ts
@@ -0,0 +1,4 @@
+import "./styles.less";
+
+
+console.log("Hello");
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: "./site/index.ts",
+    mode: process.env.MODE || 'development',
+    output: {
+        path: path.resolve(__dirname, "public")
+    },
+    module: {
+        rules: [
+            { test: /\.ts?$/, loader: "ts-loader", exclude: [/node_modules/] },
+            { test: /\.js?$/, loader: "babel-loader", exclude: [/node_modules/] },
+			{ test: /\.less$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "less-loader" }] }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "site/index.html")
+		})
+	],
+    devServer: {
+        contentBase: path.resolve(__dirname, "public"),
+        compress: false
+    }
+};
+
+
+module.exports = config;
