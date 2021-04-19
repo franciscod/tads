854c788b890049a88c3193a78c9daf2f386149b8
Francisco Demartino <demartino.francisco@gmail.com>
2021-04-19 17:35:57 -0300

    usar int.tad para los tests de eval

--- tests/OhmEvalsTxt.test.ts
+++ tests/OhmEvalsTxt.test.ts
@@ -7,45 +7,36 @@ import ohm from "ohm-js";
 
 const BOOL_TAD = fs.readFileSync("tads/bool.tad", "utf-8");
 const NAT_TAD = fs.readFileSync("tads/nat.tad", "utf-8");
+const INT_TAD = fs.readFileSync("tads/int.tad", "utf-8");
 const CONJ_TAD = fs.readFileSync("tads/conj.tad", "utf-8");
 
 const boolTad = parseTad(BOOL_TAD)!;
 const natTad = parseTad(NAT_TAD)!;
+const intTad = parseTad(INT_TAD)!;
 const conjTad = parseTad(CONJ_TAD)!;
 
 // TODO: hacer algo un poco mas prolijo que juntar todas las operaciones?
-const ops = boolTad.operaciones.concat(natTad.operaciones).concat(conjTad.operaciones);
+const ops = [
+    ...boolTad.operaciones,
+    ...natTad.operaciones,
+    ...intTad.operaciones,
+    ...conjTad.operaciones,
+];
 const [generated, unaries] = genGrammar("bool", ops, new Map());
 const g = ohm.grammar(generated);
-
-let enBool = true;
-let enNat = false;
-let enConj = false;
 
 // console.log(generated);
 
 let axiomas: Axioma[];
 
 it("parsea los axiomas", () => {
-    axiomas = auxAxiomasAST([boolTad, natTad, conjTad]);
+    axiomas = auxAxiomasAST([boolTad, natTad, intTad, conjTad]);
 });
 
 fs.readFileSync("tests/evals.txt", "utf-8")
     .replace(/\r\n/g, "\n")
     .split("\n")
     .forEach((line, n) => {
-        if (line.includes("nat")) {
-            enBool = false;
-            enNat = true;
-        }
-
-        if (line.includes("conj")) {
-            enNat = false;
-            enConj = true;
-        }
-
-        if (!enBool && !enNat && !enConj) return;
-
         line = line.split("--")[0];
         if (!line) return;
         const parts = line.split(" = ");
